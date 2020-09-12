b7e151be33037276042ded68d48f3db72e98b58c
Leandroidex <leandroomez682@gmail.com>
2020-09-12 12:38:43 -0500

    Delete Ejecutar.bat

--- Leandro Gómez Flores/Programas/Calculadora Basic/dist/Ejecutar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-color 2
-cd G:\Universidad\Semestre 5\Programación paralela\GitHub\PPS6\Leandro Gómez Flores\Programas\Calculadora Basic\dist\
-java -jar Calculadora_Basic.jar
-pause
-exit
