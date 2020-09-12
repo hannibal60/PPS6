091e33af85c1fc56dea0683550c0197233512480
Leandroidex <leandroomez682@gmail.com>
2020-09-12 12:34:34 -0500

    Calculadora basica

---
+++ Leandro Gómez Flores/Programas/Calculadora Basic/dist/Ejecutar.bat
@@ -0,0 +1,6 @@
+@echo off
+color 2
+cd G:\Universidad\Semestre 5\Programación paralela\GitHub\PPS6\Leandro Gómez Flores\Programas\Calculadora Basic\dist\
+java -jar Calculadora_Basic.jar
+pause
+exit
